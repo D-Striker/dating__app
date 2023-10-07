fa60f626a658e06205b087b7aeb0c6f3f070192a
Alexei_Bolobin <55701732+D-Striker@users.noreply.github.com>
2023-10-07 13:16:02 +0500

    ggggg

--- components/sliders.js
+++ components/sliders.js
@@ -1,26 +1,26 @@
 export const sliders = [
     {
-        id: '2',
+        id: '1',
         image: require('../img/1.jpg'),
     },
     {
-        id: '3',
+        id: '2',
         image: require('../img/2.jpg'),
     },
     {
-        id: '4',
+        id: '3',
         image: require('../img/3.jpg'),
     },
     {
-        id: '5',
+        id: '4',
         image: require('../img/4.jpg'),
     },
     {
-        id: '6',
+        id: '5',
         image: require('../img/5.jpg'),
     },
     {
-        id: '7',
+        id: '6',
         image: require('../img/6.jpg'),
     },
 ];
