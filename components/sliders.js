90a201481b0b4a96de31262af23016e32cc8cb93
Alexei_Bolobin <55701732+D-Striker@users.noreply.github.com>
2023-10-07 13:05:07 +0500

    images

--- components/sliders.js
+++ components/sliders.js
@@ -1,38 +1,26 @@
 export const sliders = [
     {
-        id: '1',
-        image: require('../img/9.jpg'),
-    },
-    {
         id: '2',
-        image: require('../img/10.avif'),
+        image: require('../img/1.jpg'),
     },
     {
         id: '3',
-        image: require('../img/11.avif'),
+        image: require('../img/2.jpg'),
     },
     {
         id: '4',
-        image: require('../img/12.avif'),
+        image: require('../img/3.jpg'),
     },
     {
         id: '5',
-        image: require('../img/13.avif'),
+        image: require('../img/4.jpg'),
     },
     {
         id: '6',
-        image: require('../img/14.avif'),
+        image: require('../img/5.jpg'),
     },
     {
         id: '7',
-        image: require('../img/15.avif'),
-    },
-    {
-        id: '8',
-        image: require('../img/16.avif'),
-    },
-    {
-        id: '9',
-        image: require('../img/17.avif'),
+        image: require('../img/6.jpg'),
     },
 ];
