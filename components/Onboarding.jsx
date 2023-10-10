389ff3e5b93e3ccc629fda660721b7158ca47780
tw1ster4k <macbookpro@MacBook-Pro-Macbook.local>
2023-10-10 15:52:42 +0200

    Release2.3.0

--- components/Onboarding.jsx
+++ components/Onboarding.jsx
@@ -8,23 +8,12 @@ export default function Onboarding() {
         <View style={styles.item__slide}>
         <ScrollView horizontal={true} pagingEnabled={true} showsHorizontalScrollIndicator={false} >
             {sliders && sliders.map((item, index) =>
-<<<<<<< HEAD
-                <View key={index} style={{overflow:"hidden"}}>
-                    {Platform.OS === "web" ? <img src={item.image} style={{ width: 400}}/> : 
-                    <Image resizeMode="contain" source={item.image} style={{minWidth: '100%',}} />}
-=======
                 <View key={index}  >
                     {Platform.OS === "web" ? <img src={item.image} style={{height:"100%", width:"auto"}} /> : <Image source={item.image} style={{width:283, height:"90%", borderRadius:25}} />}
                     <Text style={{textAlign:'center',letterSpacing:2,textTransform: 'uppercase',fontWeight: 'bold',}} >{item.name}</Text>
->>>>>>> ca91fbb1c8f0e6d4be01a003901c3baca7b3862d
                 </View>
                 )
             }
         </ScrollView>
-<<<<<<< HEAD
-)}
-
-=======
             </View>
 )}
->>>>>>> ca91fbb1c8f0e6d4be01a003901c3baca7b3862d
